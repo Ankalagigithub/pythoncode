e5db3e541e47b62c19457558c675430482b5bfe2
Ankalagigithub <138390083+Ankalagigithub@users.noreply.github.com>
2023-07-09 18:14:25 +0530

    Add files via upload

---
+++ operator in python.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#arthamatic operator
+a=10
+b=30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+print(a//b)
+
+
+# In[2]:
+
+
+#comparision operator
+a = 10
+b = 5
+print(a<b)
+print(a<=b)
+print(a>b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+
+
+# In[3]:
+
+
+#logical operator
+print(a and b)
+print(a or b)
+print(a<b and a>b)
+print(a<b or a>b)
+print(a>b or a<b)
+print(not(a<b and a>b))
+print(a>b or a<b)
+
+    
+
+
+# In[4]:
+
+
+#bitwise operator
+#bitwise and
+print(5 & 6)
+
+
+# In[5]:
+
+
+#bitwise or
+print(5 ! 6)
+
+
+# In[7]:
+
+
+print(5 | 6)
+print(5 ^ 6)
+print(~5)
+print(15>>1)
+print(15<<1)
+
+
+# In[11]:
+
+
+#assignment operator
+#identity operator
+a = 3
+b = 5
+print(a is b)
+print(a is not b)
+
+
+# In[12]:
+
+
+#membership operator
+var = "india is great"
+print("great" in var)
+print("hello" in var)    
+print("great" not in var)
+print("hello" not in var)
+
+
+# In[ ]:
+
+
+
+
