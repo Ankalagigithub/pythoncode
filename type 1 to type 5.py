41b12f10bbc787a0962549d1b579e6765807d5a5
Ankalagigithub <138390083+Ankalagigithub@users.noreply.github.com>
2023-08-12 21:58:38 +0530

    Add files via upload

---
+++ type 1 to type 5.py
@@ -0,0 +1,272 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a="septmber"
+2:-3:-1 empty
+1:-1:3 em
+5:-6:-1 bmt
+
+
+# In[4]:
+
+
+a="welcome_to_taj"
+print(a[3:8])
+print(a[5:3])
+print(a[5:-5])
+print(a[:6])
+print(a[1:])
+print(a[:-7])
+print(a[6:9])
+print(a[-3:9])
+print(a[-9:-1])
+print(a[4:-4])
+
+
+# In[2]:
+
+
+a="computer_science"
+print(a[3:10:1])
+print(a[3:10:3])
+print(a[4:-4:3])
+print(a[5:6:1])
+print(a[9:2:1])
+print(a[9:-2:1])
+print(a[:1:2])
+print(a[1::2])
+print(a[1:-2:3])
+print(a[3:-3:4])
+
+
+# In[7]:
+
+
+a="full_stack_python"
+print(a[3:-10:1])
+print(a[3:-10:3])
+print(a[4:-4:3])
+print(a[5:6:1])
+print(a[-9:2:1])
+print(a[-9:-2:-1])
+print(a[:1:-2])
+print(a[-1::2])
+print(a[1:-2:-3])
+print(a[-3:3:-4])
+
+
+# In[8]:
+
+
+a="basavaraj_ankalagi"
+print(a[6::3])
+print(a[6::-3])
+print(a[:8:3])
+print(a[:8:-3])
+print(a[2::5])
+print(a[10::5])
+print(a[:6:1])
+print(a[:7:-2])
+print(a[1::2])
+print(a[9::-9])
+print(a[3::-6])
+
+
+# In[9]:
+
+
+a="internet_pack"
+print(a[::1])
+print(a[::-1])
+print(a[::2])
+print(a[::-2])
+print(a[::-5])
+print(a[::-9])
+print(a[::6])
+print(a[::-3])
+print(a[::7])
+print(a[::-12])
+
+
+# In[2]:
+
+
+a="hello welcome to python programming"
+#capitalize()
+a.capitalize()
+
+
+# In[3]:
+
+
+#center
+b="hello"
+print(b)
+
+
+# In[4]:
+
+
+b.center(11,"x")
+
+
+# In[7]:
+
+
+#count()
+a.count("p")
+
+
+# In[10]:
+
+
+#endswith()
+a.endswith("g",0,len(a))
+
+
+# In[11]:
+
+
+a.endswith("hon",0,len(a))
+
+
+# In[12]:
+
+
+#find()
+a.find("m",0,len(a))
+
+
+# In[13]:
+
+
+a.find("w",0,len(a))
+
+
+# In[14]:
+
+
+#index()
+a.index("l",0,len(a))
+
+
+# In[15]:
+
+
+a.index("m",0,len(a))
+
+
+# In[17]:
+
+
+#isslower()
+print(a)
+a.islower()
+
+
+# In[18]:
+
+
+#upper()
+a.upper()
+
+
+# In[19]:
+
+
+#isupper()
+a.isupper()
+
+
+# In[20]:
+
+
+c="PYTHON"
+print(c)
+c.isupper()
+
+
+# In[21]:
+
+
+#istitle()
+d="Hello Welcome"
+print(d)
+
+d.istitle()
+
+
+# In[22]:
+
+
+#len()
+len(a)
+
+
+# In[23]:
+
+
+f="HELLO PROGRAM"
+print(f)
+len(f)
+
+
+# In[24]:
+
+
+#lower()
+f.lower()
+
+
+# In[25]:
+
+
+#max()
+max(c)
+
+
+# In[26]:
+
+
+#min()
+min(a)
+
+
+# In[27]:
+
+
+#startswith()
+a.startswith("hello",0,len(a))
+
+
+# In[28]:
+
+
+#startswith
+a.startswith("programming",0,len(a))
+
+
+# In[29]:
+
+
+#swapcase()
+x="fAmIlY"
+print(x)
+x.swapcase()
+
+
+# In[30]:
+
+
+#title()
+a.title()
+
+
+# In[ ]:
+
+
+
+
